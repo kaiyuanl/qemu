740b3d2433975781d63b76528a3e7daed849df14
Kaiyuan Liang <liangkaiyuan@hotmail.com>
2014-11-26 09:49:21 +0800

    add some comments and formate files

--- demo/des_demo/des_demo.c
+++ demo/des_demo/des_demo.c
@@ -18,43 +18,23 @@ volatile unsigned int * const UART0DR = (unsigned int *) UART16550_BASEADDR;
 
 void print_uart0(volatile char *s)
 {
-	while(*s != '\0') 
-	{ /* Loop until end of string */
-	    *UART0DR = (unsigned int)(*s); /* Transmit char */
-	    s++; /* Next char */
+    while(*s != '\0') 
+    {
+        *UART0DR = (unsigned int)(*s);
+        s++;
     }
 }
-
-void hex_to_string(volatile unsigned int hex, char *str)
-{
-	int i;
-	unsigned char num;
-	//unsigned int divisor;
-	unsigned int quotient;
-	//divisor = 1;
-
-	for(i = 0; i < 8; i++){
-		quotient = (char)(hex>>(4*i));
-		num = quotient%16;
-		if(num >= 10)	str[7-i] = num-10+'a';
-		else str[7-i] = num+'0';
-		//if(i < 8) divisor *= 16;
-	}
-	str[8] = '\0';
-	//return str;
-}
-
 
 void main()
 {
     volatile unsigned int* reg_key_addr = (volatile unsigned int*)(DES_BASEADDR + REG_KEY_ADDR_OFFSET);
-	volatile unsigned int* reg_in_addr = (volatile unsigned int*)(DES_BASEADDR + REG_IN_ADDR_OFFSET);
-	volatile unsigned int* reg_out_addr = (volatile unsigned int*)(DES_BASEADDR + REG_OUT_ADDR_OFFSET);
-	volatile unsigned int* reg_mode = (volatile unsigned int*)(DES_BASEADDR + REG_MODE_OFFSET);
-	
+    volatile unsigned int* reg_in_addr = (volatile unsigned int*)(DES_BASEADDR + REG_IN_ADDR_OFFSET);
+    volatile unsigned int* reg_out_addr = (volatile unsigned int*)(DES_BASEADDR + REG_OUT_ADDR_OFFSET);
+    volatile unsigned int* reg_mode = (volatile unsigned int*)(DES_BASEADDR + REG_MODE_OFFSET);
+    
     unsigned char key[9];
-	unsigned char* plain = "hello, world";
-	unsigned char cipher[OUT_SIZE_MAX];
+    unsigned char* plain = "hello, world";
+    unsigned char cipher[OUT_SIZE_MAX];
     unsigned char decrypted[OUT_SIZE_MAX];
     unsigned char* pstr;
 
@@ -64,26 +44,23 @@ void main()
     print_uart0("\n");
 
 
-	/*Generate key*/
+    /*Generate key*/
     print_uart0("start generating key\n");
     *reg_key_addr = key;
     *reg_mode = GENERATEKEY_MODE;
-	while(*reg_mode != DONE_MODE ) {
-        //break;
-	}
+    while(*reg_mode != DONE_MODE ) {}
     pstr = (unsigned char*)(*reg_key_addr);
-    print_uart0("key is");
+    print_uart0("key is ");
     print_uart0(pstr);
-	print_uart0("\n");
+    print_uart0("\n");
 
     /*Encryption*/
     print_uart0("start encrypting\n");
     *reg_in_addr = plain;
     *reg_out_addr = cipher;
     *reg_mode = ENCRYPTION_MODE;
-    while(*reg_mode != DONE_MODE) {
-    }
-	pstr = (unsigned char*)(*reg_out_addr);
+    while(*reg_mode != DONE_MODE) {}
+    pstr = (unsigned char*)(*reg_out_addr);
     print_uart0("cipher text is ");
     print_uart0(pstr);
     print_uart0("\n");
@@ -93,10 +70,9 @@ void main()
     *reg_in_addr = cipher;
     *reg_out_addr = decrypted;
     *reg_mode = DECRYPTION_MODE;
-    while(*reg_mode != DONE_MODE) {
-    }
+    while(*reg_mode != DONE_MODE) {}
     pstr = (unsigned char*)(*reg_out_addr);
-    print_uart0("decrypted text is");
+    print_uart0("decrypted text is ");
     print_uart0(pstr);
     print_uart0("\n");
 }
